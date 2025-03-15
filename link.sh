b5f7a72b9837cfc5997bda40623e320eeb1127f5
luckman212 <1992842+luckman212@users.noreply.github.com>
2025-03-15 19:50:29 -0400

    update

--- link.sh
+++ link.sh
@@ -1,5 +1,12 @@
 #!/bin/zsh --no-rcs
 
-mkdir -p /usr/local/bin
-chown $USER:staff /usr/local/bin
-chmod 0755 /usr/local/bin
+case $1 in
+	create_symlink)
+		ln -sf $PWD/pathfind.sh $dest_dir/pathfind
+		;;
+	create_usrlocalbin)
+		mkdir -p /usr/local/bin
+		chown $USER:staff /usr/local/bin
+		chmod 0755 /usr/local/bin
+		;;
+esac
