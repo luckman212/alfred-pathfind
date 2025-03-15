fc7fa90933d82638982c30f5fd22ac06097d7571
luckman212 <1992842+luckman212@users.noreply.github.com>
2025-03-15 19:50:29 -0400

    update

--- install_cli.sh
+++ install_cli.sh
@@ -1,26 +1,31 @@
 #!/bin/zsh --no-rcs
 
-dest_dir=/usr/local/bin
+export dest_dir=/usr/local/bin
 PROMPT="$alfred_workflow_name is trying to install its commandline tool at $dest_dir"
 
 if [[ ! -d $dest_dir ]]; then
 	osascript <<-EOS
-	do shell script "sh link.sh $USER" with administrator privileges with prompt "$PROMPT"
+	do shell script "sh link.sh create_usrlocalbin" with administrator privileges with prompt "$PROMPT"
 	EOS
 fi
 
-if ln -sf $PWD/pathfind.sh $dest_dir/pathfind ; then cli_result=1; else
+if ! ln -sf $PWD/pathfind.sh $dest_dir/pathfind 2>/dev/null; then
 	osascript <<-EOS
-	do shell script "'ln -sf $PWD/pathfind.sh $dest_dir/pathfind'" with administrator privileges with prompt "$PROMPT"
+	do shell script "sh link.sh create_symlink" with administrator privileges with prompt "$PROMPT"
 	EOS
-	(( $? != 0 )) && cli_result=0
+fi
+
+if [[ -L $dest_dir/pathfind ]] && [[ -x $dest_dir/pathfind ]]; then
+	cli_result=1
+else
+	cli_result=0
 fi
 
 cat <<EOJ
 {
   "alfredworkflow": {
     "variables": {
-      "cli_result": ${cli_result:-2}
+      "cli_result": $cli_result
     }
   }
 }
